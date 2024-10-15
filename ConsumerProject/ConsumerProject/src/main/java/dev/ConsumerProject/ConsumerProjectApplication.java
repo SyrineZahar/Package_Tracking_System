cba8f64476c466c7e8625c99091e8f480153798a
SyrineZahar <cyrine.zahaar@gmail.com>
2024-10-15 15:18:23 +0100

    consumer consuming notification web service

--- ConsumerProject/ConsumerProject/src/main/java/dev/ConsumerProject/ConsumerProjectApplication.java
+++ ConsumerProject/ConsumerProject/src/main/java/dev/ConsumerProject/ConsumerProjectApplication.java
@@ -13,9 +13,13 @@ public class ConsumerProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerProjectApplication.class, args);
 		Client client=ClientBuilder.newClient();
-		String result=client.target("http://localhost:8081/service/colis/123").
-				request(MediaType.APPLICATION_JSON).get(String.class);
-		System.out.println(result);
+		//String resultPackage=client.target("http://localhost:8081/service/colis/123").
+			//	request(MediaType.APPLICATION_JSON).get(String.class);
+		//System.out.println(resultPackage);
+		
+		String resultNotif=client.target("http://127.0.0.1:8000/mail?email=cyrine.zahaar@gmail.com").
+			request(MediaType.APPLICATION_JSON).get(String.class);
+		System.out.println(resultNotif);
 	}
 
 }
